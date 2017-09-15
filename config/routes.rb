620e696650465346f053df08393424e437c787a8
sortilegio <johannacastro8407@gmail.com>
2017-09-15 12:12:35 -0500

    Crea la ruta/products/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/products', to: 'products#index'
+  get '/products/new', to: 'products#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
