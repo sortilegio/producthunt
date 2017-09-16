56516be099a7030f959284c71ef88ca6b540ac69
sortilegio <johannacastro8407@gmail.com>
2017-09-16 14:14:16 -0500

    Crea la ruta PATCH /products/:id

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,15 @@ class ProductsController < ApplicationController
   def edit
     @product = Product.find(params[:id])
   end
-  
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path, notice: "El producto ha sido modificado con éxito"
+    else
+      reder :edit
+    end
+  end
   private
     def product_params
       params.require(:product).permit(:name, :url, :description)
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post '/products', to: 'products#create'
   get '/products/:id', to: 'products#show', as: 'product'
   get '/products/:id/edit', to: 'products#edit'
+  patch '/products/:id', to: 'products#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
