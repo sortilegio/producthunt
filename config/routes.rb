b3f348a25b3f718df99628786172ce8dc1cfb800
sortilegio <johannacastro8407@gmail.com>
2017-09-16 18:05:51 -0500

    Implementa la funcionalidad de autenticarse

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
-  # get '/products', to: 'products#index'
-  # get '/products/new', to: 'products#new', as: 'new_product'
-  # post '/products', to: 'products#create'
-  # get '/products/:id', to: 'products#show', as: 'product'
-  # get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  # patch '/products/:id', to: 'products#update'
-  # delete '/products/:id', to: 'products#destroy'
-
-  resources :products
   root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
+  resources :products
+
 
 end
