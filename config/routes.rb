09efc0954e9fb152bed5451283258bd7f321ad64
sortilegio <johannacastro8407@gmail.com>
2017-09-16 14:45:55 -0500

    Reemplaza las rutas por resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new', as: 'new_product'
-  post '/products', to: 'products#create'
-  get '/products/:id', to: 'products#show', as: 'product'
-  get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
-  patch '/products/:id', to: 'products#update'
-  delete '/products/:id', to: 'products#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/products', to: 'products#index'
+  # get '/products/new', to: 'products#new', as: 'new_product'
+  # post '/products', to: 'products#create'
+  # get '/products/:id', to: 'products#show', as: 'product'
+  # get '/products/:id/edit', to: 'products#edit', as: 'edit_product'
+  # patch '/products/:id', to: 'products#update'
+  # delete '/products/:id', to: 'products#destroy'
+
+  resources :products
+
 end
